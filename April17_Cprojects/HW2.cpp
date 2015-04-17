b396006a65848d34ab50f66beaeb26d68467eaa4
leejinho <leejh2773@naver.com>
2015-04-17 12:36:47 +0900

    Create HW2.cpp

---
+++ April17_Cprojects/HW2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	float k, r, a, f, d, kt, rt, at, ft;
+	k=300;
+	r=11.9*(1/3600);
+	a=850;
+	f=350;
+	d=75000000
+	kt=d/k;
+	rt=d/r;
+	at=d/a;
+	ft=d/f;
+	printf("KTX는 화성까지 총 %f 시간이 걸립니다.\n", kt);
+	printf("총
+	
