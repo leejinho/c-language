6115b934887b3057d8a63fada53444c8b5c7b66a
leejinho <leejh2773@naver.com>
2015-04-19 01:30:04 +0900

    Update HW2.cpp

--- April17_Cprojects/HW2.cpp
+++ April17_Cprojects/HW2.cpp
@@ -1,16 +1,12 @@
 #include <stdio.h>
 void main()
 {
-	float k, r, a, f, d, kt, rt, at, ft;
+	float k, r, a, f, d;
 	k=300;
-	r=11.9*(1/3600);
+	r=11.9*3600;
 	a=850;
 	f=350;
 	d=75000000
-	kt=d/k;
-	rt=d/r;
-	at=d/a;
-	ft=d/f;
-	printf("KTX는 화성까지 총 %f 시간이 걸립니다.\n", kt);
-	printf("총
+	printf("KTX는 화성까지 총 %f 시간이 걸립니다.\n", d/k);
+	printf("총 %.f년 %.f일 %.f시간 %.f분 %.f 초 가 걸립니다.\n", d/k/24/365, 
 	
