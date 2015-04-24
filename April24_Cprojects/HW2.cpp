b6c597025bb07de1904c43284cca624c64089a6a
leejinho <leejh2773@naver.com>
2015-04-24 11:41:38 +0900

    Update HW2.cpp

--- April24_Cprojects/HW2.cpp
+++ April24_Cprojects/HW2.cpp
@@ -4,16 +4,16 @@ void main()
  int y;
  printf("연도를 입력하세요\n");
  scanf("%d",&y);
-if(y%4==0)
+	if(y%4==0)
  {
-  printf("%d는 윤년입니다.\n",y);
+ 	printf("%d는 윤년입니다.\n",y);
  }
-else if((y%100!=0)&&(y%400==0))
-{
-	printf("%d는 윤년입니다.\n",y);
-}
- else
+	else if((y%100!=0)&&(y%400==0))
  {
-  printf("%d는 평년입니다.\n",y);
+	printf("%d는 윤년입니다.\n",y);
+ }
+	else
+ {
+	printf("%d는 평년입니다.\n",y);
  }
 }
