37ebd5812ad238552f6202bd21cd9a3ed8a8a6f8
leejinho <leejh2773@naver.com>
2015-04-24 11:18:24 +0900

    Update HW1.cpp

--- April24_Cprojects/HW1.cpp
+++ April24_Cprojects/HW1.cpp
@@ -4,15 +4,15 @@ void main()
 	float alcol;
 	printf("혈중 알코올 농도를 입력한뒤 Enter>");
 	scanf("%f", &alcol);
-	if(0<alcol<0.049)
+	if(alcol<=0.049&&alcol>0)
 	{
 	printf("훈방조치 입니다.\n");
 	}
-	else if(0.05<alcol<0.099)
+	else if(alcol<=0.099&&alcol>=0.05)
 	{
 	printf("면허 정지, 벌금 100만원 입니다.\n");
 	}
-	else if(0.1<alcol<0.19)
+	else if(alcol<=0.19&&alcol>=0.1)
 	{
 	printf("면허 취소, 벌금 300만원 입니다.\n");
 	}
