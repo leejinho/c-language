9f8a520eb43d33a918e72964f55c648c7fb1904f
Easyno <leejh2773@naver.com>
2015-04-28 15:12:37 +0900

    Update HW2.cpp

--- April24_Cprojects/HW2.cpp
+++ April24_Cprojects/HW2.cpp
@@ -4,11 +4,11 @@ void main()
  int y;
  printf("연도를 입력하세요\n");
  scanf("%d",&y);
-	if(y%4==0)
+	if(y%4==0, y%100!==0)
  {
  	printf("%d는 윤년입니다.\n",y);
  }
-	else if((y%100!=0)&&(y%400==0))
+	else if((y%100=0)&&(y%400==0))
  {
 	printf("%d는 윤년입니다.\n",y);
  }
