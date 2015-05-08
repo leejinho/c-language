4ec993d8d524264246782efa5d4d51135d33dd67
Easyno <leejh2773@naver.com>
2015-05-08 12:09:20 +0900

    Update 2.cpp

--- Mar8_Cprojects/2.cpp
+++ Mar8_Cprojects/2.cpp
@@ -1,10 +1,19 @@
 #include <stdio.h>
-void main()
+int main()
 {
 	int n, a, b, c, d, e, f, g=0, h=1;
+	while(1)
+	{
+	printf("15111826 이진호\n");
+	printf(" ----------------------------------------\n");
+	printf(" 1:	8-15 로 이동                           \n");
+	printf(" 2:	8-16 로 이동                            \n");
+	printf(" 3:	8-20 로 이동                          \n");
+	printf(" 4:	종료                            \n");
+	printf(" ----------------------------------------\n");
 	printf("실습 번호 입력후 Enter>>");
 	scanf("%d", &n);
-	switch(n)
+	switch(n) 
 	{
 	case 1:
 		printf("예제8-15\n");
@@ -15,7 +24,6 @@ void main()
 			g+=a;
 		printf("1부터 %d까지의 합: %d\n", b, g);
 		break;
-
 	case 2:
 		printf("예제8-16\n");
 		printf("1부터 d까지의 곱 계산\n");
@@ -35,5 +43,8 @@ void main()
 			printf("\n");
 		}
 		break;
+	case 4:
+		return 1;
+	}
 	}
 }
