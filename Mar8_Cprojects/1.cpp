ab58384a4855b5d2a82f91691ac96aa176aefbb6
Easyno <leejh2773@naver.com>
2015-05-08 10:41:14 +0900

    Create 1.cpp

---
+++ Mar8_Cprojects/1.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int main()
+{
+	int n, a, b, c, d=1, e, f, g;
+	printf("실습 번호 입력후 Enter>>");
+	scanf("%d", &n);
+	switch(n)
+	{
+	case 1: 
+		printf("예제8-4\n");
+		for (a=1;a<=10;a+=2)
+		printf("%d, 이름:이진호\n", a);
+		break;
+	case 2:
+		printf("예제8-7(1)\n");
+		for(b=1;b<=10;b+=1)
+		printf("%d\n", b);
+		break;
+	case 3: 
+		printf("예제8-7(2)\n");
+		for(c=10;c>=1;c-=1)
+		printf("%d\n", c);
+		break;
+	case 4: 
+		printf("예제8-11(1)\n");
+		while(d<=10)
+	{
+		printf("%d\n", d);
+		d+=2;
+	}
+		break;
+	case 5:
+		printf("예제8-11(2)\n");
+		for(e=1;e<=10;e+=2)
+		printf("%d\n", e);
+		break;
+	case 6: 
+		printf("예제8-12(1)\n");
+		do
+	{
+		printf("0을 입력하면 종료함\n");
+		printf("숫자 입력후 Enter>>\n");
+		scanf("%d", &f);
+	} while(f!=0);
+	break;
+	case 7: 
+		printf("예제8-12(2)\n");
+		while(g!=0)
+	{	
+		printf("0을 입력하면 종료함\n");
+		printf("숫자 입력후 Enter>>\n");
+		scanf("%d", &g);
+	}
+	break;
+ }
+}
